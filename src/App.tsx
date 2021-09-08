d8ae7f3238c4b0669e53e2d9d2949c47a5b2a320
Luciano Ricardo <lucianoricardo737@gmail.com>
2021-09-08 00:10:17 -0300

    fix eslint and add home component for test

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
 import * as React from 'react'
+import Home from './components/Home'
 import './App.css'
+
+import styled from 'styled-components'
+
+const StyleDiv = styled.div`
+display: flex;
+justify-content:space-evenly;
+align-items: center;
+color: white;
+background-color: #131A22;
+width: 100%;
+height: 100vh;
+padding: 2em;
+box-sizing: border-box;
+font-size: 5em;
+font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif ;
+`
 
 function App() {
   return (
-    <div>
-      <h3>Hola Luciano</h3>
-    </div>
+    <StyleDiv>
+      <Home />
+    </StyleDiv>
   )
 }
 
---
+++ src/components/Home.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+const Home = () => {
+  console.log('test')
+
+  return (
+    <div>
+      Welcome home
+    </div>
+  )
+}
+
+export default Home
