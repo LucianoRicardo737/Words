6e1c48764330b3982bd6d3404bf74342b62a4b25
Luciano Ricardo <lucianoricardo737@gmail.com>
2021-09-07 23:32:33 -0300

    add typescript, eslint and syle-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import * as React from 'react'
 import './App.css'
 
 function App() {
   return (
     <div>
-     <h3>Hola Luciano</h3>
+      <h3>Hola Luciano</h3>
     </div>
   )
 }
